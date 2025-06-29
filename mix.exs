5814275fc4bf0f2f2312cbc1735ed4ba94757010
Nightingale <esteticwence@gmail.com>
2025-06-29 03:47:42 +0300

    add pbkdf2 dependency
    It is used by auth.

--- mix.exs
+++ mix.exs
@@ -57,7 +57,8 @@ defmodule Spokesman.MixProject do
       {:gettext, "~> 0.26"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
-      {:bandit, "~> 1.5"}
+      {:bandit, "~> 1.5"},
+      {:pbkdf2_elixir, "~> 2.3"}
     ]
   end
 
